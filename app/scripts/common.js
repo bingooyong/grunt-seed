38d67e9b7d48c41ed26cc83bde00acde8199c195
LvYong <lvyong1985@hotmail.com>
2014-01-07 16:35:54 +0800

    mod baseUrl to absolute url

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -2,13 +2,12 @@
 
 require.config({
     urlArgs: 'rand=' + (new Date()).getTime(),// for development\
-    baseUrl: 'scripts',
+    baseUrl: '/scripts',
     paths: {
         'angular': '../bower_components/angular/angular',
         'ngResource': '../bower_components/angular-resource/angular-resource',
         'ngCookies': '../bower_components/angular-cookies/angular-cookies',
         'jQuery': '../bower_components/jquery/jquery'
-
     },
     shim: {
         ngResource: {
